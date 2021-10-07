fe0c41f58795557f7969920d47adecdfde82fa8f
OriginCode <self@origincode.me>
2021-10-06 18:19:17 -0700

    feat: impl fancy output, write a simple doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,127 @@
-use std::collections::VecDeque;
+//! A player queue for games
+//!
+//! # Examples
+//!
+//! You can create a queue by using the associated function `Queue::new()`.
+//!
+//! ```
+//! use arcade_queue::Queue;
+//! 
+//! let q = Queue::new("", 1).unwrap();
+//! ```
+
+use std::{collections::VecDeque, fmt};
 
 mod macros;
 
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub struct Queue<'a> {
-    id: u32,
-    game: String,
-    players: u8,
+    pub game: &'a str,
+    pub players: u8,
     queue: VecDeque<&'a str>,
 }
 
-impl<'a> PartialEq for Queue<'a> {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
+#[derive(Debug)]
+pub enum Error {
+    TooLessPlayersError(String),
+}
+
+impl<'a> fmt::Display for Queue<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} ({} player(s) each round): {}",
+            self.game, self.players, self.get_queue()
+        )
     }
 }
 
 impl<'a> Queue<'a> {
-    fn new(id: u32, game: &str, players: u8, queue: VecDeque<&'a str>) -> Self {
-        Self {
-            id,
-            game: game.to_owned(),
-            players,
-            queue,
+    /// Creates an empty queue with a game name and a number of players for each round.
+    /// 
+    /// `players` cannot be less than 1.
+    /// 
+    /// # Examples
+    /// 
+    /// Basic usage:
+    /// 
+    /// ```
+    /// use arcade_queue::Queue;
+    /// 
+    /// let q = Queue::new("", 1).unwrap();
+    /// ```
+    pub fn new(game: &'a str, players: u8) -> Result<Self, Error> {
+        if players == 0 {
+            return Err(Error::TooLessPlayersError(
+                "`players` should be at least 1".to_owned(),
+            ));
         }
+        Ok(Self {
+            game,
+            players,
+            queue: VecDeque::new(),
+        })
     }
 
+    /// Appends a player to the end of the queue.
+    /// 
+    /// # Examples
+    /// 
+    /// Basic usage:
+    /// 
+    /// ```
+    /// use arcade_queue::Queue;
+    /// 
+    /// let mut q = Queue::new("", 1).unwrap();
+    /// 
+    /// q.append("player1");
+    /// q.append("player2");
+    /// 
+    /// assert_eq!(q.get_queue(), "player1 player2");
+    /// ```
     #[inline]
-    fn append(&mut self, player: &'a str) {
+    pub fn append(&mut self, player: &'a str) {
         self.queue.push_back(player);
     }
-    
+
+    /// Yields the next one player.
+    /// 
+    /// # Examples
+    /// 
+    /// Basic usage:
+    /// 
+    /// ```
+    /// use arcade_queue::Queue;
+    /// 
+    /// let mut q = Queue::new("", 1).unwrap();
+    /// 
+    /// q.append("player1");
+    /// q.append("player2");
+    /// 
+    /// assert_eq!(q.nextone().unwrap(), "player1");
+    /// ```
     #[inline]
-    fn nextone(&mut self) -> Option<&'a str> {
+    pub fn nextone(&mut self) -> Option<&'a str> {
         self.queue.pop_front()
     }
 
-    fn next(&mut self) -> Vec<&'a str> {
+    /// Yields the next group of players.
+    /// 
+    /// # Examples
+    /// 
+    /// Basic usage:
+    /// 
+    /// ```
+    /// use arcade_queue::Queue;
+    /// let mut q = Queue::new("", 2).unwrap();
+    /// 
+    /// q.append("player1");
+    /// q.append("player2");
+    /// q.append("player3");
+    /// 
+    /// assert_eq!(q.next(), vec!["player1", "player2"]);
+    /// ```
+    pub fn next(&mut self) -> Vec<&'a str> {
         let mut result = Vec::new();
         for _ in 0..self.players {
             if let Some(p) = self.nextone() {
@@ -45,49 +130,67 @@ impl<'a> Queue<'a> {
         }
         result
     }
+
+    /// Get the current queue.
+    /// 
+    /// # Examples
+    /// 
+    /// Basic usage:
+    /// 
+    /// ```
+    /// use arcade_queue::Queue;
+    /// 
+    /// let mut q = Queue::new("", 1).unwrap();
+    /// 
+    /// q.append("player1");
+    /// q.append("player2");
+    /// q.append("player3");
+    /// 
+    /// assert_eq!(q.get_queue(), "player1 player2 player3")
+    /// ```
+    pub fn get_queue(&self) -> String {
+        let mut s = String::new();
+        for p in self.queue.iter() {
+            s.push_str(p);
+            s.push(' ');
+        }
+        s.pop();
+        s
+    }
 }
 
 #[cfg(test)]
 mod queue_tests {
     use crate::*;
-    #[test]
-    fn test_new() {
-        assert_eq!(
-            Queue::new(0, "maimai", 2, vecdeque!["test"]),
-            Queue {
-                id: 0,
-                game: "maimai".to_owned(),
-                players: 2,
-                queue: vecdeque!["test"],
-            }
-        );
-    }
 
     #[test]
-    fn test_append() {
-        let mut queue = Queue::new(0, "maimai", 2, vecdeque!["player1", "player2"]);
-        queue.append("OriginCode");
+    fn test_append() -> Result<(), Error> {
+        let mut queue = Queue::new("", 1)?;
+        queue.append("player");
         assert_eq!(
             queue,
             Queue {
-                id: 0,
-                game: "maimai".to_owned(),
-                players: 2,
-                queue: vecdeque!["player1", "player2", "OriginCode"],
+                game: "",
+                players: 1,
+                queue: vecdeque!["player"],
             }
         );
+        Ok(())
     }
 
     #[test]
     fn test_nextone() {
-        let mut queue = Queue::new(0, "maimai", 2, vecdeque!["player1"]);
+        let mut queue = Queue {
+            game: "test",
+            players: 2,
+            queue: vecdeque!["player1"],
+        };
         assert_eq!(queue.nextone(), Some("player1"));
         assert_eq!(queue.nextone(), None);
         assert_eq!(
             queue,
             Queue {
-                id: 0,
-                game: "maimai".to_owned(),
+                game: "test",
                 players: 2,
                 queue: VecDeque::new(),
             }
@@ -96,18 +199,45 @@ mod queue_tests {
 
     #[test]
     fn test_next() {
-        let mut queue = Queue::new(0, "maimai", 2, vecdeque!["player1", "player2", "player3"]);
+        let mut queue = Queue {
+            game: "test",
+            players: 2,
+            queue: vecdeque!["player1", "player2", "player3"],
+        };
         assert_eq!(queue.next(), vec!["player1", "player2"]);
         assert_eq!(queue.next(), vec!["player3"]);
         assert_eq!(queue.next(), Vec::<&str>::new());
         assert_eq!(
             queue,
             Queue {
-                id: 0,
-                game: "maimai".to_owned(),
+                game: "test",
                 players: 2,
                 queue: VecDeque::new(),
             }
         );
+    }
+
+    #[test]
+    fn test_get_queue() -> Result<(), Error> {
+        let mut queue = Queue::new("test", 2)?;
+        queue.append("player1");
+        queue.append("player2");
+        assert_eq!(
+            queue.get_queue(),
+            "player1 player2"
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_fmt() -> Result<(), Error> {
+        let mut queue = Queue::new("test", 2)?;
+        queue.append("player1");
+        queue.append("player2");
+        assert_eq!(
+            format!("{}", queue),
+            "test (2 player(s) each round): player1 player2"
+        );
+        Ok(())
     }
 }
